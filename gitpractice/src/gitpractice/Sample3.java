79839be3fe703d05ef1ae12788a970e8e65cd1c6
reshma <kolkarkreshma@gmail.com>
2025-07-08 18:12:42 +0530

    5th commit

--- gitpractice/src/gitpractice/Sample3.java
+++ gitpractice/src/gitpractice/Sample3.java
@@ -4,7 +4,14 @@ public class Sample3 {
 	public static void main(String[] args) {
 		System.out.println("how are you");
 		System.out.println("Everythig is fine");
+
 		//biuygtydftrdgfccvuyhbnoiutrterszfx
+                //biuygtydftrdgfccv
+		//biuygtydftrdgfccvuyhbnoiutrterszfx
+		//biuygtydftrdgfccvuyhbnoiutrterszfx        
+
+                //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa		
 		
 	}
 
