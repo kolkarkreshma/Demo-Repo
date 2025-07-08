898e9455d9576ded5a565123c776070f9a4df6ed
reshma <kolkarkreshma@gmail.com>
2025-07-08 16:49:53 +0530

    3rd commit

--- gitpractice/src/gitpractice/Sample1.java
+++
@@ -1,8 +0,0 @@
-package gitpractice;
-
-public class Sample1 {
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-
-}
--- gitpractice/src/gitpractice/Sample3.java
+++ gitpractice/src/gitpractice/Sample3.java
@@ -4,7 +4,7 @@ public class Sample3 {
 	public static void main(String[] args) {
 		System.out.println("how are you");
 		System.out.println("Everythig is fine");
-		
+		//biuygtydftrdgfccvuyhbnoiutrterszfx
 		
 	}
 
